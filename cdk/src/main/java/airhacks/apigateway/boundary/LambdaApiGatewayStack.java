35b03b88ac1ce9c51f12d4c747f3fff96ba962bc
Michael Bornholdt Nielsen <jarryDk@users.noreply.github.com>
2023-11-24 07:18:29 +0100

    Initial commit

---
+++ cdk/src/main/java/airhacks/apigateway/boundary/LambdaApiGatewayStack.java
@@ -0,0 +1,55 @@
+package airhacks.apigateway.boundary;
+
+import airhacks.InfrastructureBuilder;
+import airhacks.apigateway.control.APIGatewayIntegrations;
+import airhacks.lambda.control.QuarkusLambda;
+import software.amazon.awscdk.Stack;
+import software.constructs.Construct;
+
+public class LambdaApiGatewayStack extends Stack {
+
+
+    public static class LambdaApiGatewayBuilder{
+        private InfrastructureBuilder builder;
+        private boolean httpApiGateway;
+        
+        public LambdaApiGatewayBuilder(InfrastructureBuilder builder){
+            this.builder = builder;
+        }
+
+        LambdaApiGatewayBuilder withRestAPI(){
+            this.httpApiGateway = false;
+            return this;
+        }
+
+
+        InfrastructureBuilder infrastructureBuilder(){
+            return this.builder;
+        }
+
+        public Construct construct(){
+            return this.builder.construct();
+        }
+
+        public String stackId(){
+            return this.builder.stackId();
+        }
+
+        public LambdaApiGatewayStack build(){
+            return new LambdaApiGatewayStack(this);
+        }
+
+    }
+
+    public LambdaApiGatewayStack(LambdaApiGatewayBuilder builder) {
+        super(builder.construct(), builder.stackId());    
+        var infrastructureBuilder = builder.infrastructureBuilder();
+        var quarkuLambda = new QuarkusLambda(this,infrastructureBuilder.functionName(),infrastructureBuilder.configuration());
+        new APIGatewayIntegrations(this, builder.httpApiGateway, quarkuLambda.getFunction());
+    }
+
+
+
+
+
+}
